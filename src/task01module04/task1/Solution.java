1ab9c3468bda0a883c5836e56f4fda670e8db77e
kvr4090 <58326274+kvr4090@users.noreply.github.com>
2020-06-05 20:52:10 +0300

    Update Solution.java

--- src/task01module04/task1/Solution.java
+++ src/task01module04/task1/Solution.java
@@ -8,7 +8,7 @@ public class Solution {
 
     public double function_value(int a, int b, int c) {
         double result;
-        result = ((b + Math.sqrt(b * b + 4 * a * c)) / a * 2) - (Math.pow(a, 3) * c) + (Math.pow(b, -2));
+        result = (((b + Math.sqrt(b * b + 4 * a * c)) / (a * 2)) - (Math.pow(a, 3) * c) + (Math.pow(b, -2));
         return result;
     }
 }
