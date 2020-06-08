b632c182c2d61f6971b1d72af0efb599d1b2b054
kvr4090 <58326274+kvr4090@users.noreply.github.com>
2020-06-08 17:26:18 +0300

    Update Solution.java

--- src/task01module04/task1/Solution.java
+++ src/task01module04/task1/Solution.java
@@ -7,9 +7,13 @@ package task01module04.task1;
 public class Solution {
 
     public double function_value(int a, int b, int c) {
-        double result;
+        double result = 0;
         
-        result = ((b + Math.sqrt(b * b + 4 * a * c)) / (a * 2)) - Math.pow(a, 3) * c + Math.pow(b, -2);
+        if (a == 0) {
+            System.out.println("Деление на ноль!");
+        } else {
+            result = ((b + Math.sqrt(b * b + 4 * a * c)) / (a * 2)) - Math.pow(a, 3) * c + Math.pow(b, -2);
+        }
         
         return result;
     }
