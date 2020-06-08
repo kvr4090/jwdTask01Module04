32ad4b611a8d4d013a6b0cde15b948f123c8b7ac
kvr4090 <58326274+kvr4090@users.noreply.github.com>
2020-06-08 22:27:47 +0300

    Update Solution.java

--- src/task01module04/task2/Solution.java
+++ src/task01module04/task2/Solution.java
@@ -8,26 +8,26 @@ public class Solution {
 
     private short numberDay;
     private boolean flag = true;
-    private byte month = 1;
+    private byte currentMonth = 1;
 
     public void processingNumberDayToDate(short number) {
         numberDay = number;
 
-        day((short) 31);
-        day((short) 28);
-        day((short) 31);
-        day((short) 30);
-        day((short) 31);
-        day((short) 30);
-        day((short) 31);
-        day((short) 31);
-        day((short) 30);
-        day((short) 31);
-        day((short) 30);
-        day((short) 31);
+        month((short) 31);
+        month((short) 28);
+        month((short) 31);
+        month((short) 30);
+        month((short) 31);
+        month((short) 30);
+        month((short) 31);
+        month((short) 31);
+        month((short) 30);
+        month((short) 31);
+        month((short) 30);
+        month((short) 31);
     }
 
-    private void day(short countDays) {
+    private void month(short countDays) {
         if (numberDay <= countDays && flag) {
             System.out.println(numberDay + " - день, месяц: " + month);
             flag = false;
