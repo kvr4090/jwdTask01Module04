5776828b5b5f96a3de352289e1ac4600f231fce8
kvr4090 <58326274+kvr4090@users.noreply.github.com>
2020-06-08 22:21:20 +0300

    Update Solution.java

--- src/task01module04/task2/Solution.java
+++ src/task01module04/task2/Solution.java
@@ -6,77 +6,36 @@ package task01module04.task2;
 
 public class Solution {
 
-    public void dateByNumberDay(int number) {
-        int temp = number;
+    private short numberDay;
+    private boolean flag = true;
+    private byte counter = 1;
 
-        switch (temp / number) {
-            case 1:
-                if (temp <= 31) {
-                    System.out.println(temp + ".01");
-                    break;
-                }
-                temp -= 31;
-            case 2:
-                if (temp <= 28) {
-                    System.out.println(temp + ".02");
-                    break;
-                }
-                temp -= 28;
-            case 3:
-                if (temp <= 31) {
-                    System.out.println(temp + ".03");
-                    break;
-                }
-                temp -= 31;
-            case 4:
-                if (temp <= 30) {
-                    System.out.println(temp + ".04");
-                    break;
-                }
-                temp -= 30;
-            case 5:
-                if (temp <= 31) {
-                    System.out.println(temp + ".05");
-                    break;
-                }
-                temp -= 31;
-            case 6:
-                if (temp <= 30) {
-                    System.out.println(temp + ".06");
-                    break;
-                }
-                temp -= 30;
-            case 7:
-                if (temp <= 31) {
-                    System.out.println(temp + ".07");
-                    break;
-                }
-                temp -= 31;
-            case 8:
-                if (temp <= 31) {
-                    System.out.println(temp + ".08");
-                    break;
-                }
-                temp -= 31;
-            case 9:
-                if (temp <= 30) {
-                    System.out.println(temp + ".09");
-                    break;
-                }
-                temp -= 30;
-            case 10:
-                if (temp <= 31) {
-                    System.out.println(temp + ".10");
-                    break;
-                }
-                temp -= 31;
-            case 11:
-                if (temp <= 30) {
-                    System.out.println(temp + ".11");
-                    break;
-                }
-                temp -= 30;
-            case 12:
-                System.out.println(temp + ".12");
+    public void processingNumberDayToDate(short number) {
+        numberDay = number;
+
+        day(31);
+        day(28);
+        day(31);
+        day(30);
+        day(31);
+        day(30);
+        day(31);
+        day(31);
+        day(30);
+        day(31);
+        day(30);
+        day(31);
+    }
+
+    private void day(int countDays) {
+        if (numberDay <= countDays && flag) {
+            System.out.println(numberDay + " - день, месяц: " + counter);
+            flag = false;
         }
+
+        if (numberDay > countDays) {
+            numberDay -= countDays;
+            counter++;
+        }
+    }
 }
